ba0db426876114e6e9a00b7d2cc1272a6d417f74
kristine.dima <bruvere.kristine@gmail.com>
2021-04-09 22:17:22 +0300

    final work moved

---
+++ app/src/main/java/lv/lu/finalwork/repository/ProductRepository.java
@@ -0,0 +1,36 @@
+package lv.lu.finalwork.repository;
+
+import lv.lu.finalwork.model.Product;
+
+import java.util.List;
+import java.util.Map;
+
+public class ProductRepository implements Repository<Product> {
+
+    private Long idCounter = 0L;
+    private final Map<Long, Product> repository;
+
+    public ProductRepository(Map<Long, Product> repository) {
+        this.repository = repository;
+    }
+
+    @Override
+    public Long save(Product entity) {
+        return null;
+    }
+
+    @Override
+    public List<Product> findAll() {
+        return null;
+    }
+
+    @Override
+    public Product findById(Long id) {
+        return null;
+    }
+
+    @Override
+    public void delete(Long id) {
+
+    }
+}
